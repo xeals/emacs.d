5113ab999b04739be863c9ecfba552a0b5f67967
xeals <xeals@pm.me>
2018-10-31 22:01:35 +1100

    lang: use irony and clang-format for c modes

--- lisp/lang-c.el
+++ lisp/lang-c.el
@@ -13,34 +13,56 @@
 ;;;
 ;; Functions
 
-(defun +c-astyle-gnu-region (pmin pmax)
-  "Formats the selected region using the GNU C formatting style."
-  (interactive "r")
-  (shell-command-on-region pmin pmax
-                           "astyle -A7 -s2 -xB -xD"
-                           (current-buffer) t
-                           (get-buffer-create "*Astyle Errors*")
-                           t))
-
-(defun +c-astyle-gnu-current-buffer ()
-  (interactive)
-  (mark-whole-buffer)
-  (call-interactively #'+c-astyle-gnu-region))
+;; (defun +c-astyle-gnu-region (pmin pmax)
+;;   "Formats the selected region using the GNU C formatting style."
+;;   (interactive "r")
+;;   (shell-command-on-region pmin pmax
+;;                            "astyle -A7 -s2 -xB -xD"
+;;                            (current-buffer) t
+;;                            (get-buffer-create "*Astyle Errors*")
+;;                            t))
 
 ;;;
 ;; Packages
 
-(req-package cc-mode
-  :defer 1
+(req-package clang-format
+  :commands (clang-format-region clang-format-buffer)
+  :preface
+  (defun +clang-format-buffer-smart ()
+    "Reformat buffer if .clang-format exists in the projectile root."
+    (interactive)
+    (require 'f)
+    (when (f-exists? (expand-file-name ".clang-format" (projectile-project-root)))
+      (clang-format-buffer)))
   :general
   (:keymaps 'c-mode-map
             :states '(normal operator)
             :prefix xeal-localleader-key
-            "=" #'+c-astyle-gnu-current-buffer)
+            "=" #'+clang-format-buffer-smart)
   (:keymaps 'c-mode-map
             :states '(visual)
             :prefix xeal-localleader-key
-            "=" #'+c-astyle-gnu-region))
+            "=" #'clang-format-region))
+
+(req-package irony
+  :hook
+  (c-mode     . irony-mode)
+  (c++-mode   . irony-mode)
+  (irony-mode . irony-cdb-autosetup-compile-options))
+
+(req-package company-irony
+  :requires company
+  :commands company-irony
+  :init
+  (set-company-backends 'c-mode 'company-irony))
+
+(req-package flycheck-irony
+  :requires flycheck
+  :hook
+  (irony-mode . flycheck-mode)
+  (flycheck-mode . flycheck-irony-setup))
+
+(req-package cc-mode)
 
 (provide 'lang-c)
 ;;; lang-c.el ends here
