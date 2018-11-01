14a8c59e19e59f5d69ba5698030c463b3ac9b42d
xeals <xeals@pm.me>
2018-11-02 01:21:49 +1100

    feature: tidy hydras

--- lisp/feature-hydra.el
+++ lisp/feature-hydra.el
@@ -18,23 +18,22 @@
 ;;;
 ;; Hydras
 
-(defhydra xeal/dotfile-hydra (:color teal)
+(defhydra xeal/dotfile-hydra (:color blue)
   "Quick dotfile access."
   ("E" (find-file (concat user-emacs-directory "init.el")) "init.el")
   ("e" xeal/find-dotfiles "emacs")
   ("h" (xeal/counsel-open-config "herbstluftwm") "hlwm")
   ("z" (xeal/counsel-open-config "zsh") "zsh")
-  ("/" (counsel-find-file xdg-config-home) "search")
-  ("q" nil "cancel" :color blue))
+  ("/" (counsel-find-file xdg-config-home) "search"))
 
 (defhydra +evil/nav-hydra
   (:color red :hint nil)
   "
-^Resizing^     ^Movement^
----------------------
-   _+_            _k_
+^^Resizing^    ^^^^^Movement^
+^^^^-----------^^^^----------
+^^   _+_       ^^^     _k_
  _<_ _=_ _>_        _h_   _l_
-   _-_            _j_
+^^   _-_       ^^^     _j_
 "
   ("<" evil-window-decrease-width)
   (">" evil-window-increase-width)
@@ -50,14 +49,14 @@
 (defhydra +org/nav-hydra
   (:color red :hint nil)
   "
-      ^Navigation^          ^Subtrees^
-      ----------------------------------
-      _H_: up a level       ^        ^
-      _j_: prev heading     _<_: demote
-      _k_: next heading     _>_: promote
-      _h_: prev same level
-      _l_: next same level
-      "
+ ^Navigation^          ^Subtrees
+-----------------------------------
+ _H_: up a level
+ _j_: prev heading     _<_: demote
+ _k_: next heading     _>_: promote
+ _h_: prev same level
+ _l_: next same level
+"
   ("H" outline-up-heading)
   ("h" org-backward-heading-same-level)
   ("j" org-next-visible-heading)
