ea9658f12b3386a254aadef0756322f4149a9483
xeals <xeals@pm.me>
2018-12-29 17:32:47 +1100

    lang: use eglot for ruby

--- lisp/lang-ruby.el
+++ lisp/lang-ruby.el
@@ -12,41 +12,11 @@
 ;;;
 ;; Packages
 
-(req-package enh-ruby-mode
+(req-package ruby-mode
   :hook
-  (ruby-mode . enh-ruby-mode)
-  (enh-ruby-mode . rainbow-mode)
-  :mode (("\\.rb$"      . enh-ruby-mode)
-         ("\\.ru$"      . enh-ruby-mode)
-         ("Gemfile"     . enh-ruby-mode)
-         ("\\.gemfile$" . enh-ruby-mode)
-         ("Rakefile$"   . enh-ruby-mode)
-         ("\\.rake$"    . enh-ruby-mode)))
-
-(req-package robe
-  :hook
-  (enh-ruby-mode . robe-mode)
-  :general
-  (:keymaps 'robe-mode-map
-            :states '(normal visual operator)
-            :prefix xeal-localleader-key
-            xeal-localleader-key #'robe-start)
+  (ruby-mode . eglot-ensure)
   :init
-  ;; (defun +robe/start ()
-  ;;   (interactive)
-  ;;   (unless robe-running
-  ;;     (robe-start)))
-  ;; (defun +robe/auto-start ()
-  ;;   (interactive)
-  ;;   (unless robe-running
-  ;;     (call-interactively #'inf-ruby)))
-
-  ;; (defadvice inf-ruby-console-auto (after inf-ruby-console-auto activate)
-  ;;   "Start `robe' after the console."
-  ;;   (+robe/start))
-  ;; (defadvice inf-ruby (after inf-ruby activate)
-  ;;   (+robe/start))
-  (set-company-backends 'enh-ruby-mode 'company-robe))
+  (set-doc-fn 'ruby-mode #'eglot-help-at-point))
 
 (provide 'lang-ruby)
 ;;; lang-ruby.el ends here
