2b8310b9208c73a4b0ae62bff444ad22165a6f39
xeals <xeals@pm.me>
2018-12-29 17:40:39 +1100

    theme: summer cleaning

--- lisp/theme.el
+++ lisp/theme.el
@@ -20,41 +20,7 @@
    '(fringe ((t (:background "#1A1E24"))))
    '(ivy-posframe ((t :foreground "#cdd3df" :background "#1a1e24")))))
 
-(req-package pencil-theme
-  :el-get t :ensure t
-  :force t :demand t
-  :disabled t
-  :init
-  (setq pencil/higher-contrast-ui t
-        pencil/italics t
-        pencil/nebula t
-        pencil/variable-family xeal-variable-pitch-font
-        pencil/hide-org-block-headers nil)
-  :config
-  (load-theme 'pencil-dark t))
-
-(req-package doneburn-theme
-  :el-get t :ensure t
-  :disabled t)
-
-(req-package apropospriate-theme
-  :disabled t)
-(req-package panda-theme
-  :disabled t)
-(req-package challenger-deep-theme
-  :disabled t)
-
 (load-theme xeal-theme t)
-
-;; (req-package circadian
-;;   :demand t :force t
-;;   :disabled t
-;;   :config
-;;   (setq calendar-latitude -35.3
-;;         calendar-longitude 149.1
-;;         circadian-themes '((:sunrise . apropospriate-light)
-;;                            (:sunset  . apropospriate-dark)))
-;;   (circadian-setup))
 
 ;;;
 ;; Typography
@@ -63,8 +29,7 @@
   (with-demoted-errors "FONT ERROR: %s"
     (set-face-attribute 'default nil :height xeal-font-height :family xeal-font)
     (when xeal-variable-pitch-font
-      (set-face-attribute 'variable-pitch nil :family xeal-variable-pitch-font :height 1.2))
-    ))
+      (set-face-attribute 'variable-pitch nil :family xeal-variable-pitch-font :height 1.2))))
 
 (provide 'theme)
 ;;; theme.el ends here
