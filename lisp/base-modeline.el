536acebbc0869be73e910748aa64bd7f76470a79
xeals <xeals@pm.me>
2018-12-29 13:38:17 +1100

    modeline: use doom-modeline from melpa

--- lisp/base-modeline.el
+++ lisp/base-modeline.el
@@ -15,9 +15,17 @@
 (req-package all-the-icons
   :commands (all-the-icons-octicon all-the-icons-faicon all-the-icons-material))
 
-(require 'doom-modeline)
-
-(setq +doom-modeline-height 30)
+(req-package doom-modeline
+  :demand t
+  :hook (after-init . doom-modeline-init)
+  :init
+  (setq doom-modeline-height 30
+        doom-modeline-bar-width 3
+        doom-modeline-buffer-file-name-style 'relative-to-project
+        doom-modeline-icon t
+        doom-modeline-major-mode-icon t
+        doom-modeline-minor-modes nil
+        doom-modeline-lsp t))
 
 (provide 'base-modeline)
 ;;; base-modeline.el ends here
