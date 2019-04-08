f200f3f9cba2b87f3570c07ffd80aee2a1703908
xeals <xeals@pm.me>
2019-04-08 19:46:02 +1000

    feat(feature/evil): start eshell and term in emacs-mode

--- lisp/feature-evil.el
+++ lisp/feature-evil.el
@@ -39,6 +39,10 @@
 
   (evil-select-search-module 'evil-search-module 'evil-search)
 
+  (dolist (mode (list 'eshell-mode 'term-mode))
+    (setq evil-insert-state-modes (delete mode evil-insert-state-modes))
+    (push mode evil-emacs-state-modes))
+
   (evil-mode 1))
 
 ;; Additional objects
