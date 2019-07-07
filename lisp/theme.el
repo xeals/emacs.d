5bb008aa0019ace66e986b0a3e080ca6c8210ffb
xeals <xeals@pm.me>
2019-07-07 22:41:25 +1000

    refactor(theme): use custom-theme-set-faces for overrides

--- lisp/theme.el
+++ lisp/theme.el
@@ -23,16 +23,23 @@
    :type git
    :repo "https://gist.github.com/xeals/23ecee56bdaf14d5298a8dc15a0d8170"))
 (after! agila-theme
-  (custom-set-faces
+  (custom-theme-set-faces
+   'agila
    '(fringe ((t (:background "#1A1E24"))))
-   '(ivy-posframe ((t :foreground "#cdd3df" :background "#1a1e24"))))
-  '(solaire-default-face ((t (:inherit default :background "#252b33"))))
-  '(solaire-hl-line-face ((t (:inherit hl-line :background "#1a1e24"))))
-  '(solaire-org-hide-face ((t (:foreground "#252b33")))))
+   '(ivy-posframe ((t :foreground "#cdd3df" :background "#1a1e24")))
+   '(solaire-default-face ((t (:inherit default :background "#252b33"))))
+   '(solaire-hl-line-face ((t (:inherit hl-line :background "#1a1e24"))))
+   '(solaire-org-hide-face ((t (:foreground "#252b33"))))))
 
 ;; (load-theme xeal-theme t)
+
 (require 'metropolis-theme)
 (load-theme 'metropolis)
+(custom-theme-set-faces
+ 'metropolis
+ '(line-number ((t (:foreground "#373E35" :background "#0E1F2C"))))              ;; base03, base00
+ '(line-number-current-line ((t (:foreground "#CFD2D4" :background "#12293A")))) ;; base05, base01
+ )
 
 ;;;
 ;; Typography
